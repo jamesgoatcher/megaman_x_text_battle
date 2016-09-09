fdb2f8918b9ba291640886c3b34369ca4bda52c3
James Goatcher <jamesgoatcher@gmail.com>
2016-09-08 21:46:51 -0700

    More interaction and JS added

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
-// NOTES:
+// TO DOs:
+// Better UI design
 // GIFs smoothly animate
-// Get attack damage to be random
+// GIFs for Sigma
 // Sigma uses both attacks randomly
 
 $(document).ready(function() {
@@ -29,17 +30,23 @@ $(document).ready(function() {
 					hp:    15000,
 					atk_1: {
 									name: 'X-Buster',
-									dmg:   Math.floor(Math.random() * (999 - 600 + 1) + 600),
+									dmg:   function() { 
+										return Math.floor(Math.random() * (999 - 600 + 1) + 600);
+									},
 									count: 50
 								 },
 					atk_2: {
 									name: 'Charged Shot',
-									dmg:   Math.floor(Math.random() * (1500 - 1000 + 1) + 1000),
+									dmg:   function() {
+										return Math.floor(Math.random() * (1500 - 1000 + 1) + 1000);
+									},
 									count: 12
 								 },
 					atk_3: {
 									name: 'Giga Attack',
-									dmg:   Math.floor(Math.random() * (3300 - 2300 + 1) + 2300),
+									dmg:   function() {
+										return Math.floor(Math.random() * (3300 - 2300 + 1) + 2300);
+									},
 									count: 4
 								 },
 					low:   'It\'s time to get serious!',
@@ -51,48 +58,53 @@ $(document).ready(function() {
 							hp:    30000,
 							atk_1: {
 											name: 'Plasma Canon',
-											dmg:   Math.floor(Math.random() * (1100 - 250 + 1) + 250)
+											dmg:   function() {
+												return Math.floor(Math.random() * (900 - 250 + 1) + 250);
+											}
 										 },
 							atk_2: {
 											name: 'Energy Saber',
-											dmg:   Math.floor(Math.random() * (1700 - 1200 + 1) + 1200)
+											dmg:   function() {
+												return Math.floor(Math.random() * (2000 - 1100 + 1) + 1200);
+											}
 										 },
 							low:   'Damn you, X!',
 							dead:  false
 						},
 
+		randomAttack: function() {
+			var random = Math.floor(Math.random() * (3 - 0 + 1) + 0);
+			random;
+
+			if(random === 0) {
+				return megaMan.sigma.atk_2.dmg();
+			} else {
+				return megaMan.sigma.atk_1.dmg();
+			}
+		},
+
 		x_attack1: function() {
 			$('#x_atk1_button').on('click', function() {
-				console.log('Sigma HP:' + megaMan.sigma.hp);
-				var xLastAttack = megaMan.x.atk_1.dmg;
 
-				//HP is updated
-				document.getElementById('sigma_hp').innerHTML = (megaMan.sigma.hp -= megaMan.x.atk_1.dmg);
+				var xLastAttack = megaMan.x.atk_1.dmg();
+
+				//HP is updated, flash move and damage
+				megaMan.sigmaHPUpdate(xLastAttack);
 
 				//count is updated
-				document.getElementById('x_atk1_count').innerHTML = ((megaMan.x.atk_1.count -= 1) + '/50');
+				megaMan.xAttack1Count();
 
-				//flash red
-				$('#sigma_hp').addClass('flash_red');
-				setTimeout(function() {
-					$('#sigma_hp').removeClass('flash_red');
-				}, 250);
+				//flash text red
+				megaMan.sigmaFlashRedText();
 
-				//flash move and damage
-				document.getElementById('x_notice').innerHTML = megaMan.x.atk_1.name + '<br>' + xLastAttack;
-					setTimeout(function() {
-						document.getElementById('x_notice').innerHTML = '';
-					}, 500);
+				//sigma bg flash
+				megaMan.sigmaFlashRed();
 
 				//check win
 				megaMan.checkWin();
 
 				//prevent number below 0
-				if(!megaMan.sigma.dead){
-					setTimeout(function() {
-						megaMan.sigma_attack();
-					}, 1000);
-				};
+				megaMan.sigmaDeadNumber();
 
 				//check count
 				megaMan.checkAtk1();
@@ -102,36 +114,26 @@ $(document).ready(function() {
 
 		x_attack2: function() {
 			$('#x_atk2_button').on('click', function() {
-				console.log('Sigma HP:' + megaMan.sigma.hp);
-				var xLastAttack = megaMan.x.atk_2.dmg;
 
-				//HP is updated
-				document.getElementById('sigma_hp').innerHTML = (megaMan.sigma.hp -= megaMan.x.atk_2.dmg);
+				var xLastAttack = megaMan.x.atk_2.dmg();
+
+				//HP is updated, flash move and damage
+				megaMan.sigmaHPUpdate(xLastAttack);
 
 				//count is updated
-				document.getElementById('x_atk2_count').innerHTML = ((megaMan.x.atk_2.count -= 1) + '/12');
+				megaMan.xAttack2Count();
 
-				//flash red
-				$('#sigma_hp').addClass('flash_red');
-				setTimeout(function() {
-					$('#sigma_hp').removeClass('flash_red');
-				}, 250);
+				//flash text red
+				megaMan.sigmaFlashRedText();
 
-				//flash move and damage
-				document.getElementById('x_notice').innerHTML = megaMan.x.atk_2.name + '<br>' + xLastAttack;
-					setTimeout(function() {
-						document.getElementById('x_notice').innerHTML = '';
-					}, 500);
+				//sigma bg flash
+				megaMan.sigmaFlashRed();
 
 				//check win
 				megaMan.checkWin();
 
 				//prevent number below 0
-				if(!megaMan.sigma.dead){
-					setTimeout(function() {
-						megaMan.sigma_attack();
-					}, 1000);
-				};
+				megaMan.sigmaDeadNumber();
 
 				//check count
 				megaMan.checkAtk2();
@@ -141,36 +143,26 @@ $(document).ready(function() {
 
 		x_attack3: function() {
 			$('#x_atk3_button').on('click', function() {
-				console.log('Sigma HP:' + megaMan.sigma.hp);
-				var xLastAttack = megaMan.x.atk_3.dmg;
 
-				//HP is updated
-				document.getElementById('sigma_hp').innerHTML = (megaMan.sigma.hp -= megaMan.x.atk_3.dmg);
+				var xLastAttack = megaMan.x.atk_3.dmg();
+
+				//HP is updated, flash move and damage
+				megaMan.sigmaHPUpdate(xLastAttack);
 
 				//count is updated
-				document.getElementById('x_atk3_count').innerHTML = ((megaMan.x.atk_3.count -= 1) + '/4');
+				megaMan.xAttack3Count();
 
-				//flash red
-				$('#sigma_hp').addClass('flash_red');
-				setTimeout(function() {
-					$('#sigma_hp').removeClass('flash_red');
-				}, 250);
+				//flash text red
+				megaMan.sigmaFlashRedText();
 
-				//flash move and damage
-				document.getElementById('x_notice').innerHTML = megaMan.x.atk_3.name + '<br>' + xLastAttack;
-					setTimeout(function() {
-						document.getElementById('x_notice').innerHTML = '';
-					}, 500);
+				//sigma bg flash
+				megaMan.sigmaFlashRed();
 
 				//check win
 				megaMan.checkWin();
 
 				//prevent number below 0
-				if(!megaMan.sigma.dead){
-					setTimeout(function() {
-						megaMan.sigma_attack();
-					}, 1000);
-				};
+				megaMan.sigmaDeadNumber();
 
 				//check count
 				megaMan.checkAtk3();
@@ -179,42 +171,129 @@ $(document).ready(function() {
 		},
 
 		sigma_attack: function() {
-			console.log('X HP:' + megaMan.x.hp)
-			var sigmaLastAttack = megaMan.sigma.atk_1.dmg;
+			var sigmaLastAttack = megaMan.randomAttack();
 
-			//attack X
-			document.getElementById('x_hp').innerHTML = (megaMan.x.hp -= sigmaLastAttack);
+			//attack X, flash move and damage
+			megaMan.xHPUpdate(sigmaLastAttack);
 
 			//change x gif while attacked
-			document.getElementById('x_gif_div').innerHTML = '<img id="x_gif2" src="img/x-hurt.gif"/>';
-			setTimeout(function() {
-				document.getElementById('x_gif_div').innerHTML = '<img id="x_gif" src="img/x.gif"/>';
-			}, 250);
+			megaMan.xGifAttacked();
 
 			//flash red
-			$('#x_hp').addClass('flash_red');
-				setTimeout(function() {
-					$('#x_hp').removeClass('flash_red');
-				}, 250);
+			megaMan.xFlashRed();
 
-			//flash move and damage
-			document.getElementById('sigma_notice').innerHTML = megaMan.sigma.atk_1.name + '<br>' + sigmaLastAttack;
-				setTimeout(function() {
-					document.getElementById('sigma_notice').innerHTML = '';
-				}, 500);
+			//flash X bg red
+			megaMan.xFlashRedBg();
 
 			//check X health
-			setTimeout(function(){
-				megaMan.checkXHealth();
-			}, 251);
+			megaMan.checkXHealth();
 
 			//check Sigma health
 			megaMan.checkSigmaHealth();
 
 			//check win
-			setTimeout(function(){
-				megaMan.checkWinSigma();
-			}, 251);
+			megaMan.checkWinSigma();
+		},
+
+		sigmaHPUpdate: function(attack) {
+			console.log(attack);
+
+			document.getElementById('sigma_hp').innerHTML = (megaMan.sigma.hp -= attack);
+
+			if(attack >= 2200){
+				document.getElementById('x_notice').innerHTML = megaMan.x.atk_3.name + '<br>' + attack;
+				setTimeout(function() {
+					document.getElementById('x_notice').innerHTML = '';
+				}, 500);
+			} else if(attack <= 999) {
+				document.getElementById('x_notice').innerHTML = megaMan.x.atk_1.name + '<br>' + attack;
+				setTimeout(function() {
+					document.getElementById('x_notice').innerHTML = '';
+				}, 500);
+			} else {
+				document.getElementById('x_notice').innerHTML = megaMan.x.atk_2.name + '<br>' + attack;
+				setTimeout(function() {
+					document.getElementById('x_notice').innerHTML = '';
+				}, 500);
+			};
+			
+		},
+
+		xHPUpdate: function(attack) {
+			document.getElementById('x_hp').innerHTML = (megaMan.x.hp -= attack);
+
+			if (attack >= 1000) {
+				document.getElementById('sigma_notice').innerHTML = megaMan.sigma.atk_2.name + '<br>' + attack;
+				setTimeout(function() {
+					document.getElementById('sigma_notice').innerHTML = '';
+					var html = $('<li>').text(megaMan.sigma.atk_2.name + ', ' + attack);
+					$(html).prependTo('#sigma_console-log');
+				}, 500);
+			} else {
+				document.getElementById('sigma_notice').innerHTML = megaMan.sigma.atk_1.name + '<br>' + attack;
+				setTimeout(function() {
+					document.getElementById('sigma_notice').innerHTML = '';
+					var html = $('<li>').text(megaMan.sigma.atk_1.name + ', ' + attack);
+					$(html).prependTo('#sigma_console-log');
+				}, 500);
+			};
+		},
+
+		sigmaFlashRedText: function() {
+			$('#sigma_hp').addClass('flash_red');
+			setTimeout(function() {
+				$('#sigma_hp').removeClass('flash_red');
+			}, 250);
+		},
+
+		sigmaFlashRed: function() {
+				var sigmaDiv = document.getElementById('sigma_div_gif');
+				sigmaDiv.className += 'add_red';
+				setTimeout(function() {
+					sigmaDiv.className = '';
+				}, 250);
+		},
+
+		sigmaDeadNumber: function() {
+			if(!megaMan.sigma.dead){
+				setTimeout(function() {
+					megaMan.sigma_attack();
+				}, 1000);
+			};
+		},
+
+		xAttack1Count: function() {
+			document.getElementById('x_atk1_count').innerHTML = ((megaMan.x.atk_1.count -= 1) + '/50');
+		},
+
+		xAttack2Count: function() {
+			document.getElementById('x_atk2_count').innerHTML = ((megaMan.x.atk_2.count -= 1) + '/12');
+		},
+
+		xAttack3Count: function() {
+			document.getElementById('x_atk3_count').innerHTML = ((megaMan.x.atk_3.count -= 1) + '/4');
+		},
+
+		xGifAttacked: function() {
+			document.getElementById('x_div_gif').innerHTML = '<img id="x_gif2" src="img/x-hurt.gif"/>';
+			setTimeout(function() {
+				document.getElementById('x_div_gif').innerHTML = '<img id="x_gif" src="img/x.gif"/>';
+			}, 250);
+		},
+
+		xFlashRed: function() {
+			$('#x_hp').addClass('flash_red');
+			setTimeout(function() {
+				$('#x_hp').removeClass('flash_red');
+			}, 250);
+		},
+
+		xFlashRedBg: function() {
+				var xDiv = document.getElementById('x_div_gif');
+				xDiv.className += 'add_red';
+				setTimeout(function() {
+					xDiv.className = '';
+				}, 250);
 		},
 
 		checkWin: function() {
@@ -222,31 +301,37 @@ $(document).ready(function() {
 				megaMan.sigma.dead = true;
 				document.getElementById('sigma_hp').innerHTML = 0;
 				alert('X WINS!');
+				document.getElementById('x_notice2').innerHTML = 'X WINS!';
+				document.getElementById('sigma_notice2').innerHTML = '';
 				setTimeout(function() {
 					window.location.reload();
 				}, 5000);
 			};
-
 		},
 
 		checkXHealth: function() {
-			if(megaMan.x.hp <= 1200) {
-				document.getElementById('x_notice2').innerHTML = megaMan.x.low;
-				document.getElementById('x_gif_div').innerHTML = '<img id="x_gif2" src="img/x-low.gif"/>';
-			}
+			setTimeout(function(){
+				if(megaMan.x.hp <= 1200) {
+					document.getElementById('x_notice2').innerHTML = megaMan.x.low;
+					document.getElementById('x_div_gif').innerHTML = '<img id="x_gif2" src="img/x-low.gif"/>';
+				}
+			}, 251);
 		},
 
 		checkWinSigma: function() {
-			if(megaMan.x.hp <= 0){
-				megaMan.x.dead = true;
-				document.getElementById('x_hp').innerHTML = 0;
-				document.getElementById('x_gif_div').innerHTML = '<img id="x_gif_reverse" src="img/x-dead.gif"/>';
-				alert('SIGMA WINS!');
-				setTimeout(function() {
-					window.location.reload();
-				}, 5000);
-			};
-
+			setTimeout(function(){
+				if(megaMan.x.hp <= 0){
+					megaMan.x.dead = true;
+					document.getElementById('x_hp').innerHTML = 0;
+					document.getElementById('x_div_gif').innerHTML = '<img id="x_gif_reverse" src="img/x-dead.gif"/>';
+					alert('SIGMA WINS!');
+					document.getElementById('sigma_notice2').innerHTML = 'SIGMA WINS!';
+					document.getElementById('x_notice2').innerHTML = '';
+					setTimeout(function() {
+						window.location.reload();
+					}, 5000);
+				}
+			}, 251);
 		},
 
 		checkSigmaHealth: function() {
@@ -276,6 +361,6 @@ $(document).ready(function() {
 			}
 		}
 
-	};
+	}; //end megaMan object
 
-});
+}); //end jQuery wrap
